61dea2fbadc6acd3d99e92447ccaae2955135024
udogan <oFrJiyvd0>
2016-06-11 00:26:19 +0300

    Adding properties of Item, MailDetail, Measurement, Order, Product. Adding Product and ProductRepositroy

---
+++ src/BaklavaBorekBundle/Entity/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BaklavaBorekBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="BaklavaBorekBundle\Repository\ProductRepository")
+ */
+class Product extends CreatedUpdatedDeletedAt
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
